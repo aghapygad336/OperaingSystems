e19eaccb0b6765ab4af95581304aef32bf95ed40
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-10-29 01:37:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include<pthread.h>
 #include <time.h>
-#DEFINE CLOCKS_PER_SEC 1000
 int row , row2, column, column2;
 int twoArray[20][20];
 int twoArray_2[20][20];
@@ -26,56 +25,54 @@ void main()
 
     {
         integerArr[count]=i;
-        printf ("%d\t",  integerArr[count]);
+       // printf ("%d\t",  integerArr[count]);
         fscanf (file, "%d", &i);
 
         count ++;
     }
     fclose (file);
     int rowOfMatrixOne =integerArr[0];
-    printf("%d -> row \n",rowOfMatrixOne);
+   // printf("%d -> row \n",rowOfMatrixOne);
     int columnsOfMatrixOne =integerArr[1];
-    printf("%d -> Columns\n",columnsOfMatrixOne);
+   // printf("%d -> Columns\n",columnsOfMatrixOne);
     int lengthMatrix= (columnsOfMatrixOne*rowOfMatrixOne)+1;
 
-    printf("%d -> Size\n",lengthMatrix);
+    //printf("%d -> Size\n",lengthMatrix);
     int firstArr[25];
     for(int loop = 0; loop <lengthMatrix-1; loop++)
     {
         firstArr[loop] = integerArr[loop+2];
-        printf("%d LOOP First MAtrix \t",loop);
-        printf("%d\t", firstArr[loop]);
+        //printf("%d LOOP First MAtrix \t",loop);
+       // printf("%d\t", firstArr[loop]);
     }
 
     int stratMAtrixTwo =lengthMatrix+3;
-    printf("%d -> Start Matrix two Index \n",stratMAtrixTwo);
+   // printf("%d -> Start Matrix two Index \n",stratMAtrixTwo);
     int rowOfMatrixTwo=integerArr[stratMAtrixTwo-2];
-    printf("%d -> row Matrix two \n",rowOfMatrixTwo);
+  //  printf("%d -> row Matrix two \n",rowOfMatrixTwo);
     int columnsofMatrixTwo=integerArr[stratMAtrixTwo-1];
-    printf("%d -> row Matrix two \n",columnsofMatrixTwo);
+  //  printf("%d -> row Matrix two \n",columnsofMatrixTwo);
 
 
     int lengthMatrix2= (columnsofMatrixTwo*rowOfMatrixTwo);
 
     int secondArr[50];
     int beginL=stratMAtrixTwo;
-    printf("BEGIN MAT 2 : %d\n",beginL);
+    //printf("BEGIN MAT 2 : %d\n",beginL);
     int endArrIndex=beginL+lengthMatrix2-1;
-    printf("end MAT 2 : %d\n",endArrIndex);
-    printf("%d len :\n ",lengthMatrix2);
+    //printf("end MAT 2 : %d\n",endArrIndex);
+  // printf("%d len :\n ",lengthMatrix2);
     for(int loop2 = 0; loop2 <lengthMatrix2; loop2++)
     {
         int copyIndex =loop2+beginL;
         secondArr[loop2]=integerArr[copyIndex];
-        printf("***********************");
-        printf("   index taken from Array  %d :  %d \n  ", secondArr[loop2],loop2);
+       // printf("   index taken from Array  %d :  %d \n  ", secondArr[loop2],loop2);
 
     }
 
-    printf("^^^^^^^^^ 2D ARRAY Matrix 1^^^^^^^^^^\n");
+   // printf("^^^^^^^^^ 2D ARRAY Matrix 1^^^^^^^^^^\n");
 
     int  counter = 0;
-    int row,column;
     for ( row = 0; row < rowOfMatrixOne; row++)
     {
         for ( column  = 0; column <lengthMatrix/rowOfMatrixOne; column++)
@@ -89,29 +86,25 @@ void main()
 
 
     }
-    printf("^^^^^^^^^ 2D ARRAY Matrix 2^^^^^^^^^^\n");
-    int twoArray_2[rowOfMatrixTwo][rowOfMatrixOne];
     for ( row2 = 0; row2 < rowOfMatrixTwo; row2++)
     {
         for ( column2  = 0; column2 <lengthMatrix2/rowOfMatrixTwo; column2++)
         {
 
             twoArray_2[row2][column2] = secondArr[counter];
-            printf("%d %d%d Two D \n",twoArray_2[row2][column2],row2,column2);
+            //printf("%d %d%d Two D \n",twoArray_2[row2][column2],row2,column2);
             counter++;
         }
     }
 
     resultRow=row;
-    resultColumn=row2;
+    resultColumn=column2;
     int sizeThreads=resultRow*resultColumn;
     pthread_t tid[sizeThreads]; //->12 thread
-    void *status;
     	clock_t begin = clock();
-
+printf(" Size %d\n ",sizeThreads);
     for(int i = 0; i < sizeThreads; i++)
     {
-
 
 
 //
@@ -123,15 +116,12 @@ void main()
 //Returns 0 if OK, and non-zero (> 0) if error.
 //Parameters for the routines are passed through void * arg.
 
-        int   rc = pthread_join(thread[t], &status);
-        pthread_create(&tid[i],NULL,multiplyCell,void *p);
+         pthread_create(&tid[i],NULL,multiplyCell,NULL);
         pthread_join(tid[i], NULL);
-
+printf(" Thread Number : %d\n",i);
     }
 
 	clock_t end = clock();
-time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-printf("Time elpased is %f seconds", time_spent);
 }
 
 
@@ -139,17 +129,25 @@ printf("Time elpased is %f seconds", time_spent);
 
 void *multiplyCell(void *p)
 {
-    int  sum = 0;
 
+int shiftNum;
+int  resultOfCell = 0;
     for(int n = 0; n< resultRow; n++)
     {
         for(int r = 0; r< resultColumn; r++)
-        {
-            sum =sum+ twoArray[n][r]*twoArray_2[n][r];
+                for (int k = 0; k <column; k++) {
 
-            resultArr[n][r]=sum;
-            printf("%d",sum);
+        {
+            resultOfCell =twoArray[n][k]*twoArray_2[k][r];
+
+            resultArr[n][r]=resultOfCell;
+            printf("THE CELL IS :%d \n",resultOfCell);}
         }
+
     }
+
+
+
+
     pthread_exit(0);
 }
